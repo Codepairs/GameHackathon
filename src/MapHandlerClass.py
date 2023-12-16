b7ae5860885f49dcd57b48c14b75857cab2b91df
tssvett <tssvett@mail.ru>
2023-12-16 19:52:13 +0300

    initial commit

---
+++ src/MapHandlerClass.py
@@ -0,0 +1,6 @@
+class MapHandler:
+    """
+    MapHandler class. Make calculations to interact with map.
+    """
+    def __init__(self, game_map: str):
+        self._map = game_map
---
+++ src/ServerClass.py
@@ -0,0 +1,41 @@
+from flask import Flask, request
+from LoggerClass import Logger
+
+
+class Server:
+    """
+    Server class. Used to send and receive requests from remote GAME server.
+    """
+    def __init__(self):
+        self._logger = Logger('Server')
+        self._flask_app = Flask(__name__)
+        # Need to add requests to our server here
+        """
+        @self._flask_app.route('/bot/turn', methods=["POST"])
+        def make_turn():
+            current_game_field = request.json.get("game_field")
+            server_response = Test.make_algo_move(current_game_field, self._figure, self._opposite_figure())
+            self._logger.send_message(f"Запрос {request} был принят! Ответ сервера: {server_response}", "info")
+            return {"game_field": server_response}
+            """
+
+    def scan_request(self):
+        """
+        Scan map
+        :return:
+        """
+        pass
+
+    def shoot_request(self):
+        """
+        Shoot at smb
+        :return:
+        """
+        pass
+
+    def run(self):
+        self._logger.send_message("Сервер запущен!", "info")
+
+        #self._flask_app.run(host='127.0.0.1', port=5000)
+
+
