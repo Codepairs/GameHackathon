20c570c70b80b04c63b6d52a7e1b9c1794c3721d
tssvett <tssvett@mail.ru>
2023-12-23 19:17:32 +0300

    added all server functions

--- src/main.py
+++ src/main.py
@@ -1,7 +1,10 @@
-from ServerClass import Server
+import time
+from pprint import pprint
+from GameAPIClass import GameAPI
 
 
 if __name__ == '__main__':
-    server = Server()
-    server.run()
+    server = GameAPI()
+    server.deathmatch_registration_request()
+    server.leave_request()
 
