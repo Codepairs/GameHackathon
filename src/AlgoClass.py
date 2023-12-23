b22f52c7b1ac25ba689cdbe57d406b60715ee43d
Sergey <goosewww010@gmail.com>
2023-12-23 20:56:53 +0400

    added bot class, visualizer is in beta...

--- src/AlgoClass.py
+++ src/AlgoClass.py
@@ -3,88 +3,45 @@ import random
 
 
 class Algo:
+    @staticmethod
+    def make_move(area, coordinates, direction, speed): #speed is 1 now
+        coords = coordinates[::]
+        changing_direction_count = 0
+        while coords==coordinates:
 
-    def __init__(self, area=None, traj=0):
-        if area is None:
-            area = []
-        self.area = area   #matrix [[0, 0, 1, 1, 0], [0, 0, 0, 1, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 1], [1, 1, 1, 0, 1] ]
-        self.trajectory = traj #enum.Enum('traj', ['north', 'east', 'south', 'west'])
-        self.coordinates = [len(area[0])//2, len(area[0])//2]
-
-
-
-    def set_area(self, area):
-        self.area = area
-    def get_area(self):
-        return self.area
-
-    def choose_random_trajectory(self):
-        self.trajectory = random.randint(0, 4)
-
-    def get_trajectory(self):
-        return self.trajectory
-
-    def make_move(self): #speed is 1 now
-        coords = self.coordinates[::]
-        while coords == self.coordinates:
-
-            if self.trajectory==0:
+            if direction==0:
              #'north':
-                if (self.area[self.coordinates[0]-1][self.coordinates[1]]==0):
-                    self.coordinates[0]-=1
+                if (area[coordinates[0]-1][coordinates[1]]==0):
+                    coordinates[0]-=1
 
                 else:
-                    self.trajectory = 1
+                    direction = 1
+                    changing_direction_count+=1
 
-            if self.trajectory==1: #'east':
-                if (self.area[self.coordinates[0]][self.coordinates[1]+1] == 0):
-                    self.coordinates[1] += 1
-
-                else:
-                    self.trajectory = 2
-
-            if self.trajectory==2: #"'south':
-                if (self.area[self.coordinates[0]+1][self.coordinates[1]] == 0):
-                    self.coordinates[0] += 1
+            if direction==1: #'east':
+                if (area[coordinates[0]][coordinates[1]+1] == 0):
+                    coordinates[1] += 1
 
                 else:
-                    self.trajectory = 3
+                    direction = 2
+                    changing_direction_count+=1
 
-            if self.trajectory==3: #'west':
-                if (self.area[self.coordinates[0]][self.coordinates[1] - 1] == 0):
-                    self.coordinates[1] -= 1
+            if direction==2: #"'south':
+                if (area[coordinates[0]+1][coordinates[1]] == 0):
+                    coordinates[0] += 1
+
                 else:
-                    self.trajectory = 0
+                    direction = 3
+                    changing_direction_count+=1
 
-        return self.coordinates
+            if direction==3: #'west':
+                if (area[coordinates[0]][coordinates[1] - 1] == 0):
+                    coordinates[1] -= 1
+                else:
+                    direction = 0
+                    changing_direction_count+=1
 
-def output(matrix, coords):
-    for i in range(len(matrix[0])):
-        for j in range(len(matrix[0])):
-            if (i, j) == (coords[0], coords[1]):
-                print('X', end='')
-                continue
-            print(matrix[i][j], end='')
-        print()
-    print()
+            if changing_direction_count==4:
+                return None
 
-
-if __name__=='__main__':
-
-    matrix = [[0, 0, 1, 1, 0], [0, 0, 0, 1, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 1], [1, 1, 1, 0, 1]]
-    #print(matrix)
-    bot = Algo(matrix)
-    output(matrix, [len(matrix[0])//2, len(matrix[0])//2])
-
-    for i in range(5):
-        coords = bot.make_move()
-        output(matrix, coords)
-
-
-
-
-
-
-
-
-
+        return [coordinates, direction]
---
+++ src/BotClass.py
@@ -0,0 +1,80 @@
+import enum
+import random
+
+from AlgoClass import Algo
+
+class Direction(enum.Enum):
+    north = 0
+    east = 1
+    south = 2
+    west = 3
+
+class Bot:
+    def __init__(self, area, speed, direction, coords):
+        self.area = area
+        self.speed = speed
+        self.direction = direction
+        self.algorithm = Algo
+        self.coordinates = coords
+
+    def set_speed(self, speed):
+        self.speed = speed
+
+    def get_speed(self):
+        return self.speed
+
+    def choose_random_direction(self):
+        self.direction = random.randint(0, 4)
+
+
+    def set_direction(self, direction):
+        self.direction = direction
+
+    def get_direction(self):
+        return self.direction
+
+    def set_area(self, area):
+        self.area = area
+
+    def get_area(self):
+        return self.area
+
+    def set_coordinates(self, coords):
+        self.coordinates = coords
+
+    def get_coordinates(self):
+        return self.coordinates
+
+
+
+
+    def make_move(self):
+        coords = self.algorithm.make_move(self.area, self.coordinates, self.direction, self.speed)
+        if coords==None:
+            self.speed-=2
+        return coords
+        ''' to be continued'''
+
+def output(matrix, coords):
+    for i in range(len(matrix[0])):
+        for j in range(len(matrix[0])):
+            if (i, j) == (coords[0], coords[1]):
+                print('X', end='')
+                continue
+            print(matrix[i][j], end='')
+        print()
+    print()
+
+
+if __name__=='__main__':
+    matrix = [[0, 0, 1, 1, 0], [0, 0, 0, 1, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 1], [1, 1, 1, 0, 1]]
+    n = len(matrix[0])
+    bot = Bot(matrix, 1, 0, [n//2, n//2])
+
+    output(matrix, [len(matrix[0])//2, len(matrix[0])//2])
+
+    for i in range(5):
+        coords, new_direction = bot.make_move()
+        bot.set_coordinates(coords)
+        bot.set_direction(new_direction)
+        output(matrix, coords)
