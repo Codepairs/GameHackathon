06f80870ede327fb505295a1f2e81560f2794bb2
Sergey <goosewww010@gmail.com>
2023-12-24 11:43:08 +0400

    changed algo

--- src/AlgoClass.py
+++ src/AlgoClass.py
@@ -1,4 +1,5 @@
 import enum
+import numpy as np
 import random
 
 class Direction(enum.Enum):
@@ -10,10 +11,10 @@ class Direction(enum.Enum):
 class Algo:
     def __init__(self):
         self.possible_directions_dictionary = \
-                                    {'north': [Direction.east, Direction.west],
-                                     'east': [Direction.north, Direction.south],
-                                     'south': [Direction.east, Direction.west],
-                                     'west': [Direction.north, Direction.south]}
+                                    {'north': [Direction.west, Direction.north, Direction.east],
+                                     'east': [Direction.north, Direction.east, ],
+                                     'south': [Direction.east, Direction.south, Direction.west],
+                                     'west': [Direction.north, Direction.west, Direction.south]}
 
 
     @staticmethod
@@ -52,24 +53,142 @@ class Algo:
             return True
         return False
 
+    @staticmethod
+    def check_north_max(whole_map, ship_coordinates, ship_speed, ship_size, directions_weights):
+        direction_degrees = 0
+
+        north_free = Algo.check_north(whole_map, ship_coordinates, ship_speed * 2 + ship_size)
+        east_free = Algo.check_east(whole_map, ship_coordinates, ship_speed // 2 + ship_size)
+        west_free = Algo.check_west(whole_map, ship_coordinates, ship_speed // 2 + ship_size)
+
+        max_points = max(north_free*directions_weights[0], east_free*directions_weights[1], west_free*directions_weights[3])
+
+        if north_free*directions_weights[0]==max_points:
+            ship_speed += 4
+
+        elif (east_free*directions_weights[1]==max_points):
+            direction_degrees = 90
+            ship_speed //= 2
+
+        else:
+            direction_degrees = -90
+            ship_speed //= 2
+        ship_speed = ship_speed % 10 + ship_speed // 10 * 10
+        return [ship_speed, direction_degrees]
+
+    @staticmethod
+    def check_east_max(whole_map, ship_coordinates, ship_speed, ship_size, directions_weights):
+        direction_degrees = 0
+
+        east_free = Algo.check_east(whole_map, ship_coordinates, ship_speed * 2 + ship_size)
+        north_free = Algo.check_north(whole_map, ship_coordinates, ship_speed // 2 + ship_size)
+        south_free = Algo.check_south(whole_map, ship_coordinates, ship_speed // 2 + ship_size)
+
+        max_points = max(north_free * directions_weights[0], east_free * directions_weights[1],
+                         south_free * directions_weights[2])
+
+        if east_free * directions_weights[1] == max_points:
+            ship_speed += 4
+
+        elif (north_free * directions_weights[0] == max_points):
+            direction_degrees = -90
+            ship_speed //= 2
+
+        else:
+            direction_degrees = 90
+            ship_speed //= 2
+        ship_speed = ship_speed % 10 + ship_speed // 10 * 10
+        return [ship_speed, direction_degrees]
+
+    @staticmethod
+    def check_south_max(whole_map, ship_coordinates, ship_speed, ship_size, directions_weights):
+        direction_degrees = 0
+
+        south_free = Algo.check_south(whole_map, ship_coordinates, ship_speed * 2 + ship_size)
+        east_free = Algo.check_east(whole_map, ship_coordinates, ship_speed // 2 + ship_size)
+        west_free = Algo.check_west(whole_map, ship_coordinates, ship_speed // 2 + ship_size)
+
+        max_points = max(south_free * directions_weights[2], east_free * directions_weights[1],
+                         west_free * directions_weights[3])
+
+        if south_free * directions_weights[2] == max_points:
+            ship_speed += 4
+
+        elif (east_free * directions_weights[1] == max_points):
+            direction_degrees = -90
+            ship_speed //= 2
+
+        else:
+            direction_degrees = 90
+            ship_speed //= 2
+        ship_speed = ship_speed % 10 + ship_speed // 10 * 10
+        return [ship_speed, direction_degrees]
+
+    @staticmethod
+    def check_west_max(whole_map, ship_coordinates, ship_speed, ship_size, directions_weights):
+        direction_degrees = 0
+
+        north_free = Algo.check_north(whole_map, ship_coordinates, ship_speed * 2 + ship_size)
+        south_free = Algo.check_south(whole_map, ship_coordinates, ship_speed // 2 + ship_size)
+        west_free = Algo.check_west(whole_map, ship_coordinates, ship_speed // 2 + ship_size)
+
+        max_points = max(north_free * directions_weights[0], south_free * directions_weights[2],
+                         west_free * directions_weights[3])
+
+        if west_free * directions_weights[3] == max_points:
+            ship_speed += 4
+
+        elif (south_free * directions_weights[2] == max_points):
+            direction_degrees = -90
+            ship_speed //= 2
+
+        else:
+            direction_degrees = 90
+            ship_speed //= 2
+        ship_speed = ship_speed % 10 + ship_speed // 10 * 10
+        return [ship_speed, direction_degrees]
+
     # по сути мы знаем координаты всех островов. значит, известна зона, куда лучше не идти.
     # другими словами, не нужно вырывать часть матрицы и считать каждую клетку, достаточно проверить потенциальный ход
     # условно if whole_map[potential_move]==0: make_move
     # мы также можем потенциально заранее понять, стоит ли нам двигаться в каком-либо направлении
     # достаточно проверять зону через скорость*2
+
+    # смотрю направление движения начальное -> ПРОВЕРЯЮ ДОСТУПНЫЕ ПОВОРОТЫ И ДВИЖЕНИЯ через взвешенный массив
+    # -> проверяю их доступность
     @staticmethod
-    def make_move(whole_map, ship_coordinates, ship_direction_degrees, ship_speed, ship_size):
-        match(ship_direction_degrees):
+    def make_move_to_zone(whole_map, ship_coordinates, ship_direction, ship_speed, ship_size, directions_weights):
+        direction_degrees = 0
+        match (ship_direction):
+            case Direction.north.value:
+                return Algo.check_north_max(whole_map, ship_coordinates, ship_speed, ship_size, directions_weights)
+
+            case Direction.east.value:  # 'east':
+                return Algo.check_east_max(whole_map, ship_coordinates, ship_speed, ship_size, directions_weights)
+
+
+            case Direction.south.value:  # "'south':
+                return Algo.check_south_max(whole_map, ship_coordinates, ship_speed, ship_size, directions_weights)
+
+            case Direction.west.value:  # 'west':
+                return Algo.check_east_max(whole_map, ship_coordinates, ship_speed, ship_size, directions_weights)
+
+
+
+    @staticmethod
+    def make_move(whole_map, ship_coordinates, ship_direction, ship_speed, ship_size):
+        direction_degrees = 0
+        match(ship_direction):
             case Direction.north.value:
                 if (Algo.check_north(whole_map, ship_coordinates, ship_speed*2 + ship_size)):
                     ship_speed += 4
 
                 elif (Algo.check_east(whole_map, ship_coordinates, ship_speed//2 + ship_size)):
-                    ship_direction_degrees = 90
+                    direction_degrees = 90
                     ship_speed //= 2
 
                 elif (Algo.check_west(whole_map, ship_coordinates, ship_speed//2 + ship_size)):
-                    ship_direction_degrees = -90
+                    direction_degrees = -90
                     ship_speed //= 2
 
             case Direction.east.value:  # 'east':
@@ -77,11 +196,11 @@ class Algo:
                     ship_speed += 4
 
                 elif (Algo.check_south(whole_map, ship_coordinates, ship_speed//2 + ship_size )):
-                    ship_direction_degrees = 90
+                    direction_degrees = 90
                     ship_speed //= 2
 
                 elif (Algo.check_north(whole_map, ship_coordinates, ship_speed//2 + ship_size )):
-                    ship_direction_degrees = -90
+                    direction_degrees = -90
                     ship_speed //= 2
 
             case Direction.south.value:  # "'south':
@@ -89,11 +208,11 @@ class Algo:
                     ship_speed += 4
 
                 elif (Algo.check_west(whole_map, ship_coordinates, ship_speed//2 + ship_size)):
-                    ship_direction_degrees = 90
+                    direction_degrees = 90
                     ship_speed //= 2
 
                 elif (Algo.check_east(whole_map, ship_coordinates, ship_speed//2 + ship_size)):
-                    ship_direction_degrees = -90
+                    direction_degrees = -90
                     ship_speed //= 2
 
             case Direction.west.value:  # 'west':
@@ -101,14 +220,14 @@ class Algo:
                     ship_speed += 4
 
                 elif (Algo.check_north(whole_map, ship_coordinates, ship_speed//2 + ship_size)):
-                    ship_direction_degrees = 90
+                    direction_degrees = 90
                     ship_speed //= 2
 
                 elif (Algo.check_south(whole_map, ship_coordinates, ship_speed)):
-                    ship_direction_degrees = -90
+                    direction_degrees = -90
                     ship_speed //= 2
         ship_speed = ship_speed%10 + ship_speed//10 * 10 #check if speed is close to max
-        return [ship_speed, ship_direction_degrees]
+        return [ship_speed, direction_degrees]
 
 if __name__ == '__main__':
     print(Direction.north)
